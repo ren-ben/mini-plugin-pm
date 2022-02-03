fc756d1ffcde54099dd4f49f2c5cabef3d27136b
ren <OliGaming122@gmail.com>
2022-02-03 15:38:25 +0100

    add basic messaging functionality

--- src/main/java/com/ren/pm/ColorCodes.java
+++ src/main/java/com/ren/pm/ColorCodes.java
@@ -9,6 +9,11 @@ public class ColorCodes {
     public static String message_error = "#fc0330W#fa0435r#f8053ao#f6063fn#f50745g #f3084aS#f1094fy#ef0a54n#ed0b59t#eb0c5ea#ea0d64x#e80e69! #e60f6eT#e41073y#e21178p#e0127de " +
             "#de1382/#dd1488m#db158de#d91692s#d71797s#d5189ca#d319a1g#d11aa6e #d01bac<#ce1cb1p#cc1db6l#ca1ebba#c81fc0y#c620c5e#c521cbr#c322d0> " +
             "#c123d5<#bf24dam#bd25dfe#bb26e4s#b927e9s#b828efa#b629f4g#b42af9e#b22bfe>";
+    
+    public static String player_not_found = "#fc0330P#f7063fl#f1094da#ec0c5cy#e70e6be#e2117ar #dc1488n#d71797o#d21aa6t #cc1db4f#c720c3o#c222d2u#bd25e1n#b728efd#b22bfe!";
+    
+    public static String message_promt = "#1f6afeY#2866feo#3063feu #395ffeG#425bfeo#4a57fet #5354feA " +
+            "#5c50feN#644cfee#6d49few #7545feM#7e41fee#873efes#8f3afes#9836fea#a132feg#a92ffee#b22bfe!";
 
 //========================================================================================================================================================================
 
--- src/main/java/com/ren/pm/MessageCommand.java
+++ src/main/java/com/ren/pm/MessageCommand.java
@@ -1,5 +1,9 @@
 package com.ren.pm;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +15,15 @@ public class MessageCommand implements CommandExecutor {
         if(sender instanceof Player) {
             Player player = (Player) sender;
             if(args.length == 2) {
-
-            } else {
+                if(Bukkit.getPlayer(args[0]) != null) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    assert target != null;
+                    target.sendMessage(ChatColor.GREEN + player.getDisplayName() + ": " + args[1]);
+                    target.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("§d§lA new message appeared!"));
+                } else { //Error
+                    player.sendMessage(ColorCodes.translate(ColorCodes.player_not_found));
+                }
+            } else { //Error
                 player.sendMessage(ColorCodes.translate(ColorCodes.message_error));
             }
         }
